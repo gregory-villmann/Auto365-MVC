3f8abc548531cf9a48fdbea856d4d29be8fd6ad7
gregory-villmann <gvillmann@gmail.com>
2023-03-16 20:24:34 +0200

    Added additional CRUD routes

--- routes/cars.js
+++ routes/cars.js
@@ -60,4 +60,44 @@ router.post('/new', async (req, res) => {
     }
 });
 
+// PUT /cars/:id - edit a existing car
+router.put('/:id', async (req, res) => {
+    const id = parseInt(req.params.id)
+    const { make, model, year, mileage, price, image } = req.body;
+
+    try {
+        const car = await prisma.cars.update({
+            where: {id},
+            data: {
+                make,
+                model,
+                year: parseInt(year),
+                mileage: parseInt(mileage),
+                price: parseInt(price),
+                image,
+            },
+        });
+
+        res.status(200).json(car);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+// DELETE /cars/:id - Delete a car with the given ID
+router.delete('/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+    try {
+        const deletedCar = await prisma.cars.delete({
+            where: { id },
+        });
+
+        res.status(200).json(deletedCar);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
 module.exports = router;
